0635b6bbb1167182cb94710889e5e55f0a299b71
the static collective <joyvolcanomusic@gmail.com>
2026-08-10 10:17:18 -0500

    Export color drift generation policy

--- src/full-measure/src/generation/index.cjs
+++ src/full-measure/src/generation/index.cjs
@@ -13,4 +13,5 @@ module.exports = {
   ...require("./atmosphere-score.cjs"),
   ...require("./atmosphere-generation.cjs"),
   ...require("./possession-arc.cjs"),
+  ...require("./color-drift.cjs"),
 };
